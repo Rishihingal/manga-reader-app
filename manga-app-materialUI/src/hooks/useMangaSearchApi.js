6b05c5b6caa742134355f08f0fb005ba0e99e07d
Jasmeet Singh Khokhar <jasmeetsinghkhokhar@gmail.com>
2024-06-19 23:02:15 +0530

    Added prettier

--- manga-app-materialUI/src/hooks/useMangaSearchApi.js
+++ manga-app-materialUI/src/hooks/useMangaSearchApi.js
@@ -1,16 +1,20 @@
-import React from 'react'
+import React from "react";
 
 export const useMangaSearchApi = async (title) => {
-    const mangaList = await fetch(`https://api.mangadex.org/manga?title=${title}`).then(response => response.json()).then(data => {
-          return data.data.map(
-             manga => ({
-                 id: manga.id,
-                 title: manga.attributes.title.en,
-                 description: manga.attributes.description.en,
-                 coverArtId: manga.relationships.filter((relationship) => relationship.type === 'cover_art').map((coverArt) => coverArt.id)[0],
-             }
-          ))}).catch(error => {
-             console.log(error)
-         })
-     return mangaList
- }
+  const mangaList = await fetch(`https://api.mangadex.org/manga?title=${title}`)
+    .then((response) => response.json())
+    .then((data) => {
+      return data.data.map((manga) => ({
+        id: manga.id,
+        title: manga.attributes.title.en,
+        description: manga.attributes.description.en,
+        coverArtId: manga.relationships
+          .filter((relationship) => relationship.type === "cover_art")
+          .map((coverArt) => coverArt.id)[0],
+      }));
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  return mangaList;
+};
